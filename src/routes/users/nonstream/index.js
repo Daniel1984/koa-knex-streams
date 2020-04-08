f29443f3d51252bb0e70d6c5211c85672d9f6ee3
daniel <aciddaniel@gmail.com>
2020-04-09 00:02:58 +0300

    collect and respond version

--- src/routes/users/nonstream/index.js
+++ src/routes/users/nonstream/index.js
@@ -1,5 +1,11 @@
-module.exports = (ctx, next) => {
+const db = require('../../../services/db');
+
+module.exports = async (ctx, next) => {
   //ctx.throw(500, 'oops', { msg: 'sprry' });
+  const start = Date.now()
+  const users = await db('users').select('*');
+  const end = Date.now()
+
   ctx.status = 200;
-  ctx.body = { message: 'nostream' };
+  ctx.body = { message: 'nostream', users, time: end - start };
 };
