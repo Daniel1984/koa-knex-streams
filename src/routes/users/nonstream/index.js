ba4e172830c212a174029a33dfade4e240ba1ab0
daniel <aciddaniel@gmail.com>
2020-04-09 23:33:07 +0300

    making stream work

--- src/routes/users/nonstream/index.js
+++ src/routes/users/nonstream/index.js
@@ -2,10 +2,8 @@ const db = require('../../../services/db');
 
 module.exports = async (ctx, next) => {
   //ctx.throw(500, 'oops', { msg: 'sprry' });
-  const start = Date.now()
   const users = await db('users').select('*');
-  const end = Date.now()
 
   ctx.status = 200;
-  ctx.body = { message: 'nostream', users, time: end - start };
+  ctx.body = users;
 };
--- src/routes/users/stream/index.js
+++ src/routes/users/stream/index.js
@@ -1,7 +1,26 @@
 const fs = require('fs');
+const Stringify = require('streaming-json-stringify')
 const db = require('../../../services/db');
 
-module.exports = (ctx, next) => {
-  const stream = db.select('*').from('users').limit(10).stream();
-  ctx.body = ctx.req.pipe(stream)
+module.exports = async (ctx) => {
+  ctx.type = 'text/event-stream; charset=utf-8';
+  ctx.set('Cache-Control', 'no-cache');
+  ctx.set('Connection', 'keep-alive');
+
+  const stream = db
+    .select('*')
+    .from('users')
+    .stream();
+
+  await pipe(stream, ctx.res, { end: false });
 };
+
+function pipe(from, to, options) {
+	return new Promise((resolve, reject) => {
+    from
+      .pipe(Stringify())
+      .pipe(to, options);
+		from.on('error', reject);
+		from.on('end', resolve);
+	})
+}
